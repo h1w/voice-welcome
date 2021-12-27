03c3a95150ebe4b1f5441dabb48ec13fe9a912a8
silent_shadow <bpqvgq@gmail.com>
2021-12-27 13:32:34 +0300

    Full localtime format support

--- src/AssistantFunctions.py
+++ src/AssistantFunctions.py
@@ -1,7 +1,71 @@
 from datetime import datetime
+import time
+from utils import localtime_support_func
 
 def GetTime():
-    return datetime.now().strftime('%H часов %M минут %S секунд')
+    localtime = time.asctime( time.localtime(time.time()) )
+    correct_time = localtime.split()
+    correct_time = correct_time[3].split(':')
+
+    if (int(correct_time[0]) >= 4 and int(correct_time[0]) < 13):
+
+        if (int(correct_time[1]) == 0):
+            if(int(correct_time[0]) >= 2 and int(correct_time[0]) <= 4):
+                return "Сейчас ровно {} утра.".format(int(correct_time[0]))
+            else:
+                return "Сейчас ровно {} утра.".format(int(correct_time[0]))
+
+        else:
+            return localtime_support_func(correct_time)
+    #День
+
+    elif (int(correct_time[0]) >= 13 and int(correct_time[0]) < 17):
+
+        
+        correct_time[0] = int(correct_time[0]) - 12
+        if (int(correct_time[1]) == 0):
+            if (int(correct_time[0]) == 1):
+                return "Сейчас ровно час дня."
+            elif(int(correct_time[0]) >= 2 and int(correct_time[0]) <= 4):
+                return "Сейчас ровно {} часа дня.".format(int(correct_time[0]))
+            else:
+                return "Сейчас ровно {} часов дня.".format(int(correct_time[0]))
+
+        else: 
+            return localtime_support_func(correct_time)
+
+    #Вечер
+
+    elif (int(correct_time[0]) >= 17 and int(correct_time[0]) <= 23):
+
+        correct_time[0] = int(correct_time[0]) - 12
+
+        if (int(correct_time[1]) == 0):
+            if(int(correct_time[0]) >= 2 and int(correct_time[0]) <= 4):
+                return "Сейчас ровно {} вечера.".format(int(correct_time[0]))
+            else:
+                return "Сейчас ровно {} вечера.".format(int(correct_time[0]))
+
+        else:
+            return localtime_support_func(correct_time)
+
+    #Ночь
+
+    elif (int(correct_time[0]) >= 0 and int(correct_time[0]) < 4):
+
+        if (correct_time[0] == 0):
+            correct_time[0] = 12
+
+        if (int(correct_time[1]) == 0):
+            if (int(correct_time[0]) == 1):
+                return "Сейчас ровно час н+очи."
+            elif(int(correct_time[0]) >= 2 and int(correct_time[0]) <= 4):
+                return "Сейчас ровно {} часа н+очи.".format(int(correct_time[0]))
+            else:
+                return "Сейчас ровно {} часов н+очи.".format(int(correct_time[0]))
+
+        else:
+            return localtime_support_func(correct_time)
 
 def GetData():
     return datetime.now().strftime('%d день %m месяц %Y года')
--- src/utils.py
+++ src/utils.py
@@ -7,3 +7,103 @@ def ConvertPcmToWav(name, output_name):
     with wave.open(output_name, 'wb') as wavfile:
         wavfile.setparams((1, 2, 44100, 1, 'NONE', 'NONE'))
         wavfile.writeframes(pcmdata)
+
+def localtime_support_func(correct_time):
+    if (int(correct_time[0]) >= 5 and int(correct_time[0]) <= 20):
+        if (int(correct_time[2]) == 0):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас ровно {} часов и {} минута".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif(int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас ровно {} часов и {} минуты".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас ровно {} часов и {} минут".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+        elif ((int(correct_time[2])%10 == 1  or int(correct_time[2])%10 == 0) or (int(correct_time[2]) >= 10 and int(correct_time[2]) < 20) or (int(correct_time[2])%10 >= 5 and int(correct_time[2])%10 <= 9)):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часов, {} минута и {} секунд".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif(int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часов, {} минуты и {} секунд".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас {} часов, {} минут и {} секунд".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+        elif (int(correct_time[2])%10 == 1):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часов, {} минута и {} секунда".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif (int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часов, {} минуты и {} секунда".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас {} часов, {} минут и {} секунда".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+        elif (int(correct_time[2])%10 <= 4):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часов, {} минута и {} секунды".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif (int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часов, {} минуты и {} секунды".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас {} часов, {} минут и {} секунды".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+    if (int(correct_time[0]) == 1 or int(correct_time[0]) == 21):
+        if (int(correct_time[2]) == 0):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас ровно {} час и {} минута".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif(int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас ровно {} час и {} минуты".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас ровно {} час и {} минут".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+        elif ((int(correct_time[2])%10 == 1  or int(correct_time[2])%10 == 0) or (int(correct_time[2]) >= 10 and int(correct_time[2]) < 20) or (int(correct_time[2])%10 >= 5 and int(correct_time[2])%10 <= 9)):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} час, {} минута и {} секунд".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif(int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} час, {} минуты и {} секунд".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас {} час, {} минут и {} секунд".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+        elif (int(correct_time[2])%10 == 1):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} час, {} минута и {} секунда".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif (int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} час, {} минуты и {} секунда".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас {} час, {} минут и {} секунда".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+        elif (int(correct_time[2])%10 <= 4):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} час, {} минута и {} секунды".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif (int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} час, {} минуты и {} секунды".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас {} час, {} минут и {} секунды".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+    if ((int(correct_time[0])%10 >= 2 and int(correct_time[0]) <= 4) and (int(correct_time[0]) < 10 or int(correct_time[0]) > 20)):
+        if (int(correct_time[2]) == 0):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас ровно {} часа и {} минута".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif(int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас ровно {} часа и {} минуты".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас ровно {} часа и {} минут".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+        elif ((int(correct_time[2])%10 == 1  or int(correct_time[2])%10 == 0) or (int(correct_time[2]) >= 10 and int(correct_time[2]) < 20) or (int(correct_time[2])%10 >= 5 and int(correct_time[2])%10 <= 9)):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часа, {} минута и {} секунд".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif(int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часа, {} минуты и {} секунд".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас {} часа, {} минут и {} секунд".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+        elif (int(correct_time[2])%10 == 1):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часа, {} минута и {} секунда".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif (int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часа, {} минуты и {} секунда".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас {} часа, {} минут и {} секунда".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+
+        elif (int(correct_time[2])%10 <= 4):
+            if (int(correct_time[1])%10 == 1 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часа, {} минута и {} секунды".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            elif (int(correct_time[1])%10 >= 2 and int(correct_time[1])%10 <= 4 and (int(correct_time[1]) >= 20 or int(correct_time[1]) <= 10)):
+                return "Местное время сейчас {} часа, {} минуты и {} секунды".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
+            else:
+                return "Местное время сейчас {} часа, {} минут и {} секунды".format(int(correct_time[0]),int(correct_time[1]),int(correct_time[2]))
